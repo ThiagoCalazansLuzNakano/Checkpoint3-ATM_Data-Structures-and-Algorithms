6359c1024d7a04b50761c85ef63089207856d208
ThiagoNakano <tclnakano@gmail.com>
2026-05-19 11:50:47 -0300

    Finalização do códio-Thiago

--- main.c
+++ main.c
@@ -1,25 +1,65 @@
-#include<stdio.h>
-int main(){
-    int opcao;
-    do {
-    system("cls"); // Limpa a tela
-    exibirMenu();
-    scanf("%d", &opcao);
-    // Processamento da opcao...
-    } while(opcao != 0);
-    printf("Obrigado por usar nosso ATM!");
+#include <stdio.h>
 
-    void exibirMenu() {
-    printf("\n========================\n");
+void exibirMenu() {
+    printf("\n============================\n");
     printf(" CAIXA ELETRONICO \n");
-    printf("========================\n");
+    printf("=============================\n");
     printf("1 - Consultar Saldo\n");
     printf("2 - Realizar Saque\n");
     printf("3 - Realizar Deposito\n");
     printf("0 - Sair\n");
-    printf("========================\n");
+    printf("============================\n");
     printf("Escolha uma opcao: ");
-    }
-    
-    return 0;
+}
+float realizarDeposito(float saldo){
+    float valorDeposito;
+    float vD= valorDeposito;
+    float s= saldo;
+    printf("Valor do deposito: ");
+    scanf("%f", &vD);
+    s+=vD;
+    printf("\nDeposito feito com sucesso");
+    return s;
+}
+float realizarSaque(float saldo) {
+    float valorSaque;
+    float v= valorSaque;
+    float s= saldo;
+    printf("Valor do saque: ");
+    scanf("%f", &v);
+    if(v <= s && s > 0){
+    s -= v;
+    printf("Sucesso!\n");
+}else {
+    printf("Sem saldo!\n");
+}
+    return s;
+}
+void consultarSaldo(float saldo) {
+    printf("\n--- SALDO ATUAL ---\n");
+    printf("R$ %.2f\n", saldo);
+    printf("-------------------\n");
+}
+float realizarSaque(float s);
+int main() {
+    float saldo = 0; // 0 de saldo
+    int op;
+do {
+    exibirMenu();
+    scanf("%d", &op);
+    switch(op) {
+    case 1:
+    consultarSaldo(saldo); break;
+    case 2:
+    saldo = realizarSaque(saldo); break;
+    case 3:
+    saldo = realizarDeposito(saldo); break;
+    case 0:
+    printf("Saindo...\n"); break;
+    default: 
+    printf("Opcao Invalida!\n"); break;
+}
+} while(op != 0);
+
+return 0;
 }
