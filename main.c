4b1215dc2b8595da7ab9e28584611a5fa7ab1aeb
leticiaokano <aikuku0110@gmail.com>
2026-05-19 11:08:04 -0300

    Menu visuall - Leticia Aiko

--- main.c
+++ main.c
@@ -9,5 +9,17 @@ int main(){
     } while(opcao != 0);
     printf("Obrigado por usar nosso ATM!");
 
+    void exibirMenu() {
+    printf("\n========================\n");
+    printf(" CAIXA ELETRONICO \n");
+    printf("========================\n");
+    printf("1 - Consultar Saldo\n");
+    printf("2 - Realizar Saque\n");
+    printf("3 - Realizar Deposito\n");
+    printf("0 - Sair\n");
+    printf("========================\n");
+    printf("Escolha uma opcao: ");
+    }
+    
     return 0;
 }
